3487d636111aac8ffdc8cef057a09cc3d5746dfb
Daste <stefankar1000@gmail.com>
2022-03-21 18:27:16 +0100

    Add Typescript to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,59 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
-    bundle: './app/index.js'
+    bundle: "./app/index.ts",
   },
   output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
   },
   module: {
     rules: [
       {
-        test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/
+        test: /\.js$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(ttf|eot|woff|woff2)$/,
-        loader: 'url-loader?limit=10000&minetype=application/font-woff'
+        loader: "url-loader?limit=10000&minetype=application/font-woff",
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
-              limit: 5000
-            }
-          }
-        ]
-      }
+              limit: 5000,
+            },
+          },
+        ],
+      },
     ],
   },
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
-  ]
+      template: "app/index.html",
+    }),
+  ],
 };
